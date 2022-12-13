884b97cbc454a25eae7d78bbbca24e0dd277fdb1
burakJs <burakimdat13@gmail.com>
2022-12-13 22:16:00 +0300

    feat: add entity, dao, service and controller

---
+++ src/main/java/abg/dev/entities/concretes/Tweet.java
@@ -0,0 +1,45 @@
+package abg.dev.entities.concretes;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table(name = "tweets")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Tweet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Size(min = 2, max = 255, message = "Please enter a text within the borders")
+    @NotNull
+    @NotBlank
+    @Column(name = "tweet")
+    private String tweet;
+
+    @Column(name = "created_at")
+    private LocalDate createdAt = LocalDate.now();
+
+    @Column(name = "like_count")
+    private int likeCount = 0;
+
+    @Column(name = "user_id")
+    private int userId;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = true)
+    @JoinColumn(name = "user_id", insertable = false, updatable = false)
+    @JsonIgnore
+    private User user;
+}
